1d72e1bccdde1a9e471c8069a8f965cb3d1241b2
fmv8 <128718671+fmv8@users.noreply.github.com>
2023-03-23 15:26:05 +0000

    Added mui library and buttons

---
+++ mui-basic-app/src/Button.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+      <Button disabled variant="text">Text</Button>
+  );
+}
