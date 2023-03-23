5da4935e1cea37d1b382529ee28568bdadfce3a1
klimHLMC <128719320+klimHLMC@users.noreply.github.com>
2023-03-23 11:42:46 -0400

    Update checkboxHL.tsx

--- checkboxHL.tsx
+++ checkboxHL.tsx
@@ -3,6 +3,6 @@ import Checkbox from '@mui/material/Checkbox';
 
 export default function CheckboxesCustom() {
   return (
-      <Checkbox color="default" style={{ color: #00FF00}}/>
+      <Checkbox color="default" style={{ color: '#00FF00'}}/>
   );
 }
