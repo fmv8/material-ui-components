fef3f7eb078e41e1d62909c69b700660880175d4
klimHLMC <128719320+klimHLMC@users.noreply.github.com>
2023-03-23 11:40:53 -0400

    Create checkboxHL.tsx

---
+++ checkboxHL.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+
+export default function CheckboxesCustom() {
+  return (
+      <Checkbox color="default" style={{ color: #00FF00}}/>
+  );
+}
